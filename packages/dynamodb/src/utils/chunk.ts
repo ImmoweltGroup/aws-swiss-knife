7928f120fbcf421e337105d9e785345648b4472d
Dennis Kribl <d.kribl@immowelt.de>
2020-09-17 15:18:54 +0200

    fix(): add alternative for reduce() in chunk function
    Co-authored-by: wtho <thomas@wtho.de>

--- packages/dynamodb/src/utils/chunk.ts
+++ packages/dynamodb/src/utils/chunk.ts
@@ -1,11 +1,5 @@
-export const chunk = <T>(items: T[], size: number): T[][] =>
-  items.reduce(
-    (prev, item) => {
-      if (prev[prev.length - 1].length >= size) {
-        prev.push([]);
-      }
-      prev[prev.length - 1].push(item);
-      return prev;
-    },
-    [[]]
-  );
+export const range = (len: number) => [...Array(len).keys()]
+export const chunk = <T>(items: T[], size: number) => {
+  const numChunks = range(Math.ceil(items.length / size))
+  return numChunks.map(i => items.slice(i * size, (i + 1) * size))
+}
