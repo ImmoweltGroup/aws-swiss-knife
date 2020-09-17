120f2c190e79d5ea966ef6b1088efe1ec1221076
Kribl, Dennis <d.kribl@immowelt.de>
2020-09-17 16:08:40 +0200

    feat(): whitelist more commits prefixes
    Signed-off-by: Kribl, Dennis <d.kribl@immowelt.de>

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,10 @@ module.exports = {
   rules: {
     "type-case": [2, "always", "lower-case"],
     "type-empty": [2, "never"],
-    "type-enum": [2, "always", ["feat", "fix", "docs", "build", "chore"]],
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "docs", "build", "chore", "ci", "refactor", "test"],
+    ],
   },
 };
