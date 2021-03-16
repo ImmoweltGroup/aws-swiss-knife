3fe6532a5af704af5e291041609ac73110ca032c
Matthias Rohmer <matthias.rohmer@immowelt.de>
2021-03-16 13:05:19 +0100

    fix(dynamodb): fix cross account copy (#195)

--- packages/dynamodb/src/copy.ts
+++ packages/dynamodb/src/copy.ts
@@ -5,8 +5,21 @@ import { throwOnError } from "./operators/throw-on-aws-error";
 
 export const copy = (source: Table, destination: Table) =>
   bindCallback(copyLib as (p: any, cb: (error, result) => void) => void)({
-    config: source,
-    source,
-    destination,
+    source: {
+      tableName: source.tableName,
+      config: {
+        accessKeyId: source.accessKeyId,
+        secretAccessKey: source.secretAccessKey,
+        region: source.region,
+      },
+    },
+    destination: {
+      tableName: destination.tableName,
+      config: {
+        accessKeyId: destination.accessKeyId,
+        secretAccessKey: destination.secretAccessKey,
+        region: destination.region,
+      },
+    },
     log: true,
   }).pipe(throwOnError());
